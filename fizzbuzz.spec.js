189d949d01c1d67750d132f6e2604a5de602c9d1
hjbrown11 <hannah.j.brown@hotmail.co.uk>
2021-12-15 13:14:44 +0000

    Change wording of test

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -1,5 +1,5 @@
-const fizzbuzz = require('./fizzbuzz');
+const fizzbuzz = require("./fizzbuzz");
 
-test('returns fizz if num is multiple of 3', () => {
-    expect(fizzbuzz(3)).toBe('fizz');
+test("returns fizz if num modulus 3 is equal to 0", () => {
+  expect(fizzbuzz(3)).toBe("fizz");
 });
