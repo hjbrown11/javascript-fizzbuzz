ccf7212183e77c18284a6716b6d9c81f8bc37c3c
hjbrown11 <hannah.j.brown@hotmail.co.uk>
2021-12-15 13:41:27 +0000

    Add outcome fizzbuzz

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -5,5 +5,9 @@ test("returns fizz if num modulus 3 is equal to 0", () => {
 });
 
 test("returns buzz if num modulus 5 is equal to 0", () => {
-    expect(fizzbuzz(5)).toBe("buzz");
+  expect(fizzbuzz(5)).toBe("buzz");
+});
+
+test("returns fizzbuzz if num modulus 3 and 5 are equal to 0", () => {
+  expect(fizzbuzz(15)).toBe("fizzbuzz");
 });
